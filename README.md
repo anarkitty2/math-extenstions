1c583c629a70874e5929021d3689918744a7886c
anarkitty2 <willowzed@proton.me>
2026-03-31 21:58:10 -0400

    Added two new functions
    Get B
    Get Y
    Also separated functions for ease of readability

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Math Extensions
-
+# Constants
 PI: Constant set to 3.14159265
 
+# Misc Functions
 fact(): Method for getting factorials<br>
+mean(x: list): Method for getting mean of list of numbers
 
 # Circles and Spheres
 circumference(r): Method for getting the circumference of a circle<br>
@@ -12,6 +14,8 @@ s_area(r): Method for getting area of a sphere<br>
 
 # Linear Equations
 linequ_get_a(z: string, y): Will return x value required to get y in a linear equation as a float<br>
+linequ_get_y(z: string, x): Will return Y value of given equation and x value<br>
+linequ_get_b(z: str, y, x): Will return b value of given equation with y and x
 ## Examples
 linequ_get_a("2x+3", 11) will return 4.0<br>
 linequ_get_a("2x-1", 9) will return 5.0<br>
--- rmextensions.py
+++ rmextensions.py
@@ -11,6 +11,9 @@ def fact(n: int):
     except ZeroDivisionError: raise ZeroDivisionError("Error, area must be a valid integer or float")
     except TypeError: raise TypeError("Error, area must be a valid integer or float")
 
+"""
+Geometry
+"""
 
 def circumference(r):
     try:
@@ -32,6 +35,9 @@ def s_area(r):
         return 4*PI*(r**2)
     except ValueError: raise ValueError("Error, radius must be a valid integer or float")
 
+"""
+Stats
+"""
 
 def mean(x: list):
     z = 0
@@ -42,13 +48,20 @@ def mean(x: list):
     except TypeError: raise TypeError("Error, input must be a valid list")
 
 
-def linequ_get_a(z: str, y):
+"""
+Linear algebra
+"""
+def linequ_get_x(z: str, y):
     try:
         # making sure only valid equations exist
         if '+' not in z and '-' not in z: return ValueError("Error, invalid equation")
         if '+' in z and '-' in z: return ValueError("Error, invalid equation")
         # get a and b
         x_const = float(z[z.find('x') - 1])
+
+        #check if there is a, otherwise set x_const to 1
+        if z.find('x') - 1 == -1: x_const = 1
+        else: x_const = float(z[z.find('x') - 1])
 
         # check if - is found
         if '-' in z:
@@ -60,3 +73,39 @@ def linequ_get_a(z: str, y):
 
     except ValueError:
         return ValueError("Error, invalid equation")
+
+
+def linequ_get_y(z: str, x):
+    try:
+        if '+' not in z and '-' not in z: return ValueError("Error, invalid equation")
+        if '+' in z and '-' in z: return ValueError("Error, invalid equation")
+
+        if z.find('x') - 1 == -1: x_const = 1
+        else: x_const = float(z[z.find('x') - 1])
+
+        if '-' in z:
+            const = float(z[z.find('-') + 1])
+            return (x_const*x) - const
+        if '+' in z:
+            const = float(z[z.find('+') + 1])
+            return (x_const*x) + const
+
+
+    except: return ValueError("Error, invalid equation")
+
+
+def linequ_get_b(z: str, y, x):
+    try:
+        if '+' not in z and '-' not in z: return ValueError("Error, invalid equation")
+        if '+' in z and '-' in z: return ValueError("Error, invalid equation")
+
+        if z.find('x') == -1: a = 1
+        else: a = float(z[z.find('x') - 1])
+
+        if '-' in z:
+            return y - (a * x)
+        if '+' in z:
+            return y - (a * x)
+
+
+    except: return ValueError("Error, invalid equation")
